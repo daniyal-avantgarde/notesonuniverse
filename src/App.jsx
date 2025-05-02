72b56fee3f7b5ca35fc5ecea574bf8f1a08fbf6a
Daniyal <daniyal.avantgarde@gmail.com>
2025-05-02 23:25:45 +1000

    Completely rehauled folder. Set up Vite, React, and TailwindCSS.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="priorities">
+        <p>These are my priorities: ...</p>
+      </div>
+
+      <div className="min-h-screen bg-gradient-to-r from-pink-500 to-yellow-500 flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-white">Tailwind is working!</h1>
+      </div>
+
+      <h1>Hello world!</h1>
+      <p>Welcome to my blog - made with React!</p>
+    </div>
+  );
+}
+
+export default App;
