e9dca6dd1fefbbdd039af1ec307685ee9878727e
Daniyal <daniyal.avantgarde@gmail.com>
2025-05-14 13:58:44 +1000

    Another distraction
    The Vite folder intimidates me so I keep checking out and trying to understand everything from the ground up. I'm not going to do that anymore. Bye, this branch.

---
+++ index.js
@@ -0,0 +1,13 @@
+import ForceGraph2D from 'react-force-graph-2d';
+
+function App() {
+    return (
+        <div>
+            <h1>Notes on Universe</h1>
+            <subtitle>A blog for the bold</subtitle>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App></App>);
