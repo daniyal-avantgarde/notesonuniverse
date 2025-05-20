80e921116554acb968ef789e06e7ab8ea1a07b6c
Daniyal <daniyal.avantgarde@gmail.com>
2025-05-20 19:23:52 +1000

    Trying to get nodeData.json to be written dynamically on server start.
    Wrote a nodeDataUpdater.js script to do that. I got a little lazy and had ChatGPT write the necessary updates to my vite.config.js. I hope it comes to bite my ass later. Anyway. I need to make nodeDataUpdater.js run correctly despite it not being in the root folder, which I'll accomplish by rewiring how it accesses paths. WARNING: CODE IS DYSFUNCTIONAL. nodeDataUpdater.js NEEDS TO BE FIXED.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,39 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup';
+import mdx from '@mdx-js/rollup'
+import { exec } from 'child_process'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mdx()],
+  plugins: [
+    react(),
+    mdx(),
+    {
+      name: 'run-nodeDataUpdater',
+      apply: 'serve',
+      configureServer(server) {
+        const runScript = () => {
+          exec('node ./src/pages/nodeDataUpdater.js', (err, stdout, stderr) => {
+            if (err) {
+              console.error('Error running nodeDataUpdater.js:', err)
+              return
+            }
+            console.log('nodeDataUpdater.js output:\n', stdout || stderr)
+          })
+        }
+
+        // Run once on startup
+        runScript()
+
+        // Watch .mdx files
+        server.watcher.on('change', (file) => {
+          if (file.endsWith('.mdx')) {
+            console.log(`File changed: ${file}`)
+            runScript()
+          }
+        })
+      }
+    }
+  ]
 })
 
