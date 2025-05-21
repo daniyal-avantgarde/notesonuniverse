c742f8683385e8c32dedddfb2a5bff9ecfbe843f
Daniyal <daniyal.avantgarde@gmail.com>
2025-05-21 16:30:20 +1000

    Reverted to manually updating Graph data
    Code is functional again. Edit nodeData.json to change how nodes are displayed on the graph. I want to push to finish an MVP ASAP

--- vite.config.js
+++ vite.config.js
@@ -7,33 +7,7 @@ import { exec } from 'child_process'
 export default defineConfig({
   plugins: [
     react(),
-    mdx(),
-    {
-      name: 'run-nodeDataUpdater',
-      apply: 'serve',
-      configureServer(server) {
-        const runScript = () => {
-          exec('node ./src/pages/nodeDataUpdater.js', (err, stdout, stderr) => {
-            if (err) {
-              console.error('Error running nodeDataUpdater.js:', err)
-              return
-            }
-            console.log('nodeDataUpdater.js output:\n', stdout || stderr)
-          })
-        }
-
-        // Run once on startup
-        runScript()
-
-        // Watch .mdx files
-        server.watcher.on('change', (file) => {
-          if (file.endsWith('.mdx')) {
-            console.log(`File changed: ${file}`)
-            runScript()
-          }
-        })
-      }
-    }
+    mdx()
   ]
 })
 
