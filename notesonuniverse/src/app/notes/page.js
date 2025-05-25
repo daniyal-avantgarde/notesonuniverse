84dd401a736884f2be86d2dc032d2e1a5c76ffba
Daniyal <daniyal.avantgarde@gmail.com>
2025-05-25 12:13:36 +1000

    Initial Next.js setup
    Need to dynamically route to mdx files somehow...

---
+++ notesonuniverse/src/app/notes/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function about() {
+    return (
+        <div>
+            <h1>notes home page</h1>
+            <Link href="/">go back home</Link>
+        </div>
+    )
+}
